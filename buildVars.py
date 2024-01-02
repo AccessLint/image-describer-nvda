0a115f2a70df4b739a87e91b14f7ffece4560ffb
Cameron Cundiff <github@ckundo.com>
2024-01-01 20:24:47 -0500

    Bump version to 0.1.3

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("Describe content with AI."),
 	# version
-	"addon_version": "0.1.0",
+	"addon_version": "0.1.3",
 	# Author(s)
 	"addon_author": "Cameron Cundiff <cameron@accesslint.com>",
 	# URL for the add-on documentation support
